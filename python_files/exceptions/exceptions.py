ef2afb34427e6a39de51961a7f0546aeedc977a0
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-31 15:52:52 -0230

    learning exceptions with stacks and queues

---
+++ python_files/exceptions/exceptions.py
@@ -0,0 +1,21 @@
+import sys
+
+try:
+    f = open('myfile.txt', "r")
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS error:", err)
+except ValueError:
+    print("Could not convert data to an integer.")
+except Exception as err:
+    print(f"Unexpected {err=}, {type(err)=}")
+    raise
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
