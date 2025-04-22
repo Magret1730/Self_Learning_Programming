a51a55daa1b8df7c9e5c6506c399cbe3b15f084c
Magret <belloabiodun17@gmail.com>
2025-04-21 22:12:09 -0230

    Setup

---
+++ nextjs/traversy_property_pulse/property-pulse/postcss.config.mjs
@@ -0,0 +1,43 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   }
+// }
+
+// postcss.config.js
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     // autoprefixer: {},
+//   },
+// }
+
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    // autoprefixer: {},
+  }
+}
