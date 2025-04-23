7bcb42a746c9c76055e167a4abf57477d2cfc25d
Magret <belloabiodun17@gmail.com>
2025-04-22 21:46:09 -0230

    Add Homepage components and footer

--- nextjs/traversy_property_pulse/property-pulse/app/page.jsx
+++ nextjs/traversy_property_pulse/property-pulse/app/page.jsx
@@ -1,10 +1,12 @@
-import Link from "next/link";
+// import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
     return (
         <div>
-            <h1 className="text-3xl">Welcome</h1>
-            <Link href="/properties">Go to Properties</Link>
+            {/* <h1 className="text-3xl">Welcome</h1> */}
+            {/* <Link href="/properties">Go to Properties</Link> */}
             {/* <Link href="/properties?name=test">Go to Properties</Link> */}
             {/* <Link href={{
                 pathname: 'properties',
@@ -12,6 +14,9 @@ const HomePage = () => {
             }}>
                 Go to Properties
             </Link> */}
+
+            <Hero />
+            <InfoBoxes />
         </div>
     );
 }
