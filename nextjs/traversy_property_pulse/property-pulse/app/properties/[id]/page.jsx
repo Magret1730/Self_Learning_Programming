bdd2e23508ad05b6a86ee1dbef231f82b20f22c8
Magret <belloabiodun17@gmail.com>
2025-04-24 19:22:13 -0230

    Add properties details component

--- nextjs/traversy_property_pulse/property-pulse/app/properties/[id]/page.jsx
+++ nextjs/traversy_property_pulse/property-pulse/app/properties/[id]/page.jsx
@@ -28,12 +28,42 @@
 
 
 // SERVER COMPONENT
-const PropertyPage = ({ params, searchParams }) => {
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa"
+
+const PropertyPage = async ({ params }) => {
+    await connectDB();
+
+    const param = await params;
+
+    const property = await Property.findById(param.id);
+
     return (
-        <div>
-            Property Page {params.id}
-            {/* <p>{searchParams.name}</p> */}
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+          <section>
+            <div className="container m-auto py-6 px-6">
+              <Link
+                href="/properties"
+                className="text-blue-500 hover:text-blue-600 flex items-center"
+              >
+                <FaArrowLeft className="mr-2"/> Back to Properties
+              </Link>
+            </div>
+          </section>
+    <section className="bg-blue-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+          <PropertyDetails property={property}/>
         </div>
+      </div>
+    </section>
+        </>
+
     );
 }
 
