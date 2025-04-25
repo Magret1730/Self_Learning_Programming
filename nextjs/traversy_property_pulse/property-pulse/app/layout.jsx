41bd3307af6e65619abe800ea78bc587d1848fc7
Magret <belloabiodun17@gmail.com>
2025-04-24 22:29:20 -0230

    Implement Next Auth and Provider Setup

--- nextjs/traversy_property_pulse/property-pulse/app/layout.jsx
+++ nextjs/traversy_property_pulse/property-pulse/app/layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import '@/assets/styles/globals.css';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
     title: "Property Pulse",
@@ -9,14 +10,16 @@ export const metadata = {
 };
 
 const MainLayout = ({ children }) => {
-    return ( 
-        <html>
-            <body>
-                <Navbar />
-                <main>{ children }</main>
-                <Footer />
-            </body>
-        </html>
+    return (
+        <AuthProvider>
+            <html>
+                <body>
+                    <Navbar />
+                    <main>{ children }</main>
+                    <Footer />
+                </body>
+            </html>
+        </AuthProvider>
     );
 }
 
