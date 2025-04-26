2f7884d14129ad0acc9c8f44117de4032a15d9bb
Magret <belloabiodun17@gmail.com>
2025-04-26 09:52:49 -0230

    Add Property Form

--- nextjs/traversy_property_pulse/property-pulse/middleware.js
+++ nextjs/traversy_property_pulse/property-pulse/middleware.js
@@ -1,5 +1,8 @@
-export { default } from 'next-auth/middleware';
+// Middleware for protected routes
 
+export { default } from 'next-auth/middleware'; // thi smakes all routes protected. No hitting any route except you're logged in
+
+// Specifies the protected routes. You can only hit these set of routes except you're logged in
 export const config = {
   matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
 }
