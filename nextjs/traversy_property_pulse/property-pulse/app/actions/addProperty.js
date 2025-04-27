c800b60dbabd66d9255fa1178344f37d21b6c22e
Magret <belloabiodun17@gmail.com>
2025-04-27 15:46:33 -0230

    Cloudinary Image Upload

--- nextjs/traversy_property_pulse/property-pulse/app/actions/addProperty.js
+++ nextjs/traversy_property_pulse/property-pulse/app/actions/addProperty.js
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
 async function addProperty(formData) {
   // console.log(formData.get('name'));
@@ -23,7 +24,7 @@ async function addProperty(formData) {
   const images = formData
     .getAll("images")
     .filter((image) => image.name !== '')
-    .map((image) => image.name);
+    // .map((image) => image.name); //This only uses map to get image name
 
   const propertyData = {
     owner: userId, // This is connected to a user and knows who is submitting it
@@ -50,9 +51,33 @@ async function addProperty(formData) {
       email: formData.get("seller_info.email"),
       phone: formData.get("seller_info.phone"),
     },
-    images,
+    // images,
   };
 
+  const imageUrls = [];
+
+  // Loop through the images and convert each one
+  for (const imageFile of images) {
+    const imageBuffer = await imageFile.arrayBuffer();
+    const imageArray = Array.from(new Uint8Array(imageBuffer));
+    const imageData = Buffer.from(imageArray);
+
+    // Convert to base64
+    const imageBase64 = imageData.toString('base64');
+
+    // Make request to cloudinary by attaching each image
+    const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+      folder: 'propertyPulseTraversy'
+    });
+
+    // We get back a result object and on it is the url to access the image
+    imageUrls.push(result.secure_url);
+  }
+
+  //Adding to propertyData
+  propertyData.images = imageUrls
+
+  // Submit it to database
   const newProperty = new Property(propertyData);
   await newProperty.save();
 
