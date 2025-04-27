c3c3b22b8826f5cbd6da5b3c079ca4a2db4a95dd
Magret <belloabiodun17@gmail.com>
2025-04-27 16:44:19 -0230

    PropertyImages COmponent functionality

--- nextjs/traversy_property_pulse/property-pulse/app/properties/[id]/page.jsx
+++ nextjs/traversy_property_pulse/property-pulse/app/properties/[id]/page.jsx
@@ -32,6 +32,7 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import PropertyImages from "@/components/PropertyImages";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -43,26 +44,27 @@ const PropertyPage = async ({ params }) => {
     const property = await Property.findById(param.id);
 
     return (
-        <>
-          <PropertyHeaderImage image={property.images[0]} />
-          <section>
-            <div className="container m-auto py-6 px-6">
-              <Link
-                href="/properties"
-                className="text-blue-500 hover:text-blue-600 flex items-center"
-              >
-                <FaArrowLeft className="mr-2"/> Back to Properties
-              </Link>
+      <>
+        <PropertyHeaderImage image={property.images[0]} />
+        <section>
+          <div className="container m-auto py-6 px-6">
+            <Link
+              href="/properties"
+              className="text-blue-500 hover:text-blue-600 flex items-center"
+            >
+              <FaArrowLeft className="mr-2"/> Back to Properties
+            </Link>
+          </div>
+        </section>
+        <section className="bg-blue-50">
+          <div className="container m-auto py-10 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+              <PropertyDetails property={property}/>
             </div>
-          </section>
-    <section className="bg-blue-50">
-      <div className="container m-auto py-10 px-6">
-        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-          <PropertyDetails property={property}/>
-        </div>
-      </div>
-    </section>
-        </>
+          </div>
+        </section>
+        <PropertyImages images={property.images} />
+      </>
 
     );
 }
