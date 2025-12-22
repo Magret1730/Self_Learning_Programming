b31aad86170773b11cb894b656b95744cbac4df1
Magret <belloabiodun17@gmail.com>
2025-12-22 15:32:12 -0330

    Java learning on println and datatypes

---
+++ Java/Variables/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // String name = "Magret";
+        // int num = 23;
+        // System.out.println("Hello and welcome " + name + "!. Your age is " + num + ".");
+
+        int intMaxValue = Integer.MAX_VALUE; // This gives us max value that can be stored in int data type
+        int intMinValue = Integer.MIN_VALUE; // This gives us min value that can be stored in int data type
+        byte byteMaxValue = Byte.MAX_VALUE;  // This gives us max value that can be stored in byte data type
+        byte byteMinValue = Byte.MIN_VALUE; // // This gives us min value that can be stored in byte data type
+
+        System.out.println("Max int value: " + intMaxValue + "\nMin int value: " + intMinValue);
+        System.out.println("Max byte value: " + byteMaxValue + "\nMin byte value: " + byteMinValue);
+    }
+}
