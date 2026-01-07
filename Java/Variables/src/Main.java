ba4b808056f953050af8c4505d5ad8d156e4ac69
Magret <belloabiodun17@gmail.com>
2026-01-07 12:55:46 -0330

    Learnt Java operations and operands

--- Java/Variables/src/Main.java
+++ Java/Variables/src/Main.java
@@ -2,21 +2,76 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // DATA TYPES
         // String name = "Magret";
         // int num = 23;
         // System.out.println("Hello and welcome " + name + "!. Your age is " + num + ".");
+        // long myNum = 2147483648L; // Use "L" for long data types, if not the data type will be treated as int
+        // System.out.println("Long number: " + myNum);
+        // float floatNum = 3.14f;
+        // double doubleNum = 2.0;
+//        char letter = 'A';
+//        boolean isActive = false;
+//        System.out.println(letter);
+//        System.out.println(isActive);
 
-        int intMaxValue = Integer.MAX_VALUE; // This gives us max value that can be stored in int data type
-        int intMinValue = Integer.MIN_VALUE; // This gives us min value that can be stored in int data type
-        byte byteMaxValue = Byte.MAX_VALUE;  // This gives us max value that can be stored in byte data type
-        byte byteMinValue = Byte.MIN_VALUE; // // This gives us min value that can be stored in byte data type
-        long longMaxValue = Long.MAX_VALUE; // This gives us max value that can be stored in long data type
-        long longMinValue = Long.MIN_VALUE; // This gives us min value that can be stored in long data type
+//        int intMaxValue = Integer.MAX_VALUE; // This gives us max value that can be stored in int data type
+//        int intMinValue = Integer.MIN_VALUE; // This gives us min value that can be stored in int data type
+//        byte byteMaxValue = Byte.MAX_VALUE;  // This gives us max value that can be stored in byte data type
+//        byte byteMinValue = Byte.MIN_VALUE; // // This gives us min value that can be stored in byte data type
+//        long longMaxValue = Long.MAX_VALUE; // This gives us max value that can be stored in long data type
+//        long longMinValue = Long.MIN_VALUE; // This gives us min value that can be stored in long data type
+//        short shortMaxValue = Short.MAX_VALUE; // This gives us max value that can be stored in short data type
+//        short shortMinValue = Short.MIN_VALUE; // This gives us min value that can be stored in short data type
+//        float floatMaxValue = Float.MAX_VALUE;
+//        float floatMinValue = Float.MIN_VALUE;
+//        double doubleMaxValue = Double.MAX_VALUE;
+//        double doubleMinValue = Double.MIN_VALUE;
 
-        System.out.println("Max int value: " + intMaxValue + "\nMin int value: " + intMinValue);
-        System.out.println("Max byte value: " + byteMaxValue + "\nMin byte value: " + byteMinValue);
-        System.out.println("Max long value: " + longMaxValue + "\nMin long value: " + longMinValue);
+//        System.out.println("Max int value: " + intMaxValue + " Min int value: " + intMinValue);
+//        System.out.println("Max byte value: " + byteMaxValue + " Min byte value: " + byteMinValue);
+//        System.out.println("Max long value: " + longMaxValue + " Min long value: " + longMinValue);
+//        System.out.println("Max short value: " + shortMaxValue + " Min long value: " + shortMinValue);
+//        System.out.println("Max float value: " + floatMaxValue + " Min Float value: " + floatMinValue);
+//        System.out.println("Max double value: " + doubleMaxValue + " Min Double value: " + doubleMinValue);
 
-        long myNum = 2147483648L; // Use "L" for long data types, if not the data type will be treated as int
+
+        // SHORT CUTS
+        // 1. cmd + d = duplicates line
+        // 2. shift + ctrl + p = checks for expressions
+        // 3. shift + ctrl + p then enter gives the data type of the expression
+        // sout for System.out.println();
+
+        // NOTES
+
+        // OPERATIONS
+        int num1 = 5;
+        int num2 = 3;
+//        float num1 = 5f;
+//        float num2 = 3;
+
+//        System.out.println(num1 + num2); // num1 + num2 is an expression // num1 is an operand and + is an operator
+//        System.out.println(num1 - num2);
+//        System.out.println(num1 * num2);
+//        System.out.println(num1 / num2);
+//        System.out.println(num1 % num2); // Remainder of division of the two operands
+//        System.out.println(num1 + num2 * 4); // Follows BODMAS
+//        System.out.println((num1 + num2) * 4); // Follows BODMAS
+
+        int myNumber = 9;
+        myNumber = myNumber + 1;
+        myNumber += 1;
+        myNumber -= 4;
+        myNumber *= 4;
+        myNumber /= 2;
+        myNumber %= 3;
+
+//        System.out.println(myNumber);
+
+        myNumber = 0;
+//        System.out.println("Post incrementation" + myNumber++);
+//        System.out.println(myNumber);
+//        System.out.println("Prefix incrementation" + ++myNumber);
+//        System.out.println(myNumber);
     }
 }
