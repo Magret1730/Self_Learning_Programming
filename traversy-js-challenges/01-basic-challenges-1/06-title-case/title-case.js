c94efb6ba4c8f0a3e161a1668d8cabcce44afab9
Magret <belloabiodun17@gmail.com>
2026-04-26 19:35:13 -0230

    Title case challenge solution

--- traversy-js-challenges/01-basic-challenges-1/06-title-case/title-case.js
+++ traversy-js-challenges/01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,22 @@
-function titleCase() {}
+// PSEUDOCODE
+// 1. Get the sentence
+// 2. Split it using space
+// 3. Get the first letter of each word in the sentence already splitted
+// 4. Turn it to uppercase
+// 5. Join the words back with space
+// 6. Return it
+
+function titleCase(sentence) {
+	let words = sentence.split(" ");
+	let newWords = [];
+
+	for (i = 0; i < words.length; i++) {
+		// newWords.push(words[i].charAt(0).toUpperCase() + words[i].slice(1)); OR
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+	}
+
+	// return newWords.join(" "); OR
+  return words.join(" ");
+}
 
 module.exports = titleCase;
