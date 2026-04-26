f2aa502d3777ed3557c9b6c442e6ddb6867ef994
Magret <belloabiodun17@gmail.com>
2026-04-26 20:10:40 -0230

    Reveerse String challenge solution

--- traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,21 @@
-function reverseString() {}
+// PSEUDOCODE
+// MANUALLY
+// 1. Get the word
+// 2. Loop through the word
+// 3. Push into a new variable from the last word
+// 4. Return the word
+
+function reverseString(word) {
+  let reversedWord = [];
+
+  for (i = word.length; i >= 0; i--) {
+    reversedWord.push(word[i]);
+  }
+
+  return reversedWord.join('');
+}
+
+// BUILT-IN
+// const reverseString = (word) => word.split('').reverse().join('');
 
 module.exports = reverseString;
