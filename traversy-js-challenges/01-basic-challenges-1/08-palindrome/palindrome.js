362d7c9c13e9cedd37fc79090e5415e702ad1c96
Magret <belloabiodun17@gmail.com>
2026-04-28 11:26:37 -0230

    Palindrome challenge solution

--- traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
+++ traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,18 @@
-function isPalindrome() {}
+// PSEDOCODE
+// Get the word
+// Reverse the word
+// Compare the word with the reversed word
+// If same return true or else return false
+
+function isPalindrome(word) {
+	const normalizedWord = word.toLowerCase().replace(/[ ,]/g, "");
+	const reversedWord = word.toLowerCase().replace(/[ ,]/g, "").split("").reverse().join("");
+
+	if (normalizedWord === reversedWord) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 module.exports = isPalindrome;
